093113d429b0c40e26f3fa581bc00f5adaa06b63
BytemanD <byteman.d@outlook.com>
2025-11-23 11:30:59 +0800

    feat: 添加样例代码
    Signed-off-by: BytemanD <byteman.d@outlook.com>

--- src/pydashboard/dashboard.py
+++ src/pydashboard/dashboard.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QMainWindow, QWidget, QGridLayout
+from PyQt6.QtWidgets import QMainWindow, QWidget, QGridLayout, QLabel
+from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout
 
 
 
@@ -9,15 +10,29 @@ class Dashboard(QMainWindow):
 
         self.setWindowTitle(title)
         self.setGeometry(100, 100, 800, 600)
-        self.central_widget = QWidget(self)
+        self.central = QWidget(self)
 
-        self.central_layout = QGridLayout(self)
+        self.central_layout = QGridLayout(self.central)
 
         # Application bar
         self.app_bar = QWidget()
         self.app_bar_layout = QGridLayout(self.app_bar)
+
+        self.left = QWidget()
+        self.left_layout = QHBoxLayout(self.left)
+
+        self.content = QWidget()
+        self.content_layout = QHBoxLayout(self.content)
+
         self._init_dashboard()
 
     def _init_dashboard(self):
-        self.setCentralWidget(self.central_widget)
-        self.central_layout.addWidget(self.app_bar)
+        self.central_layout.addWidget(self.left, 0, 0)
+        self.central_layout.addWidget(self.app_bar, 0, 1, 0, 11)
+        self.central_layout.addWidget(self.content, 1, 1, 11, 11)
+
+        self.app_bar_layout.addWidget(QLabel("app bar"))
+        self.left_layout.addWidget(QLabel("left widget"))
+        self.content_layout.addWidget(QLabel("content"))
+
+        self.setCentralWidget(self.central)
